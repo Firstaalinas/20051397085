be5cd98eb9c4341bfc8caf85b36ebb6f862e369f
Firsta alina s <72842581+Firstaalinas@users.noreply.github.com>
2020-10-14 09:23:01 +0700

    Add files via upload

---
+++ firsta alina_2020A_20051397085.cpp
@@ -0,0 +1,26 @@
+/* Program Billing Warnet *//
+/* Firsta Alina Saputra (20051397085) *//
+#include <stdio.h>
+#include <conio.h>
+
+main ()
+{
+	int sisa,total_detik_mulai,total_detik_mulai
+	
+	printf ("Billing Warnet Firsta Alina\n");
+	printf ("20051397085");
+	printf ("\n \n");
+	
+	printf (MULAI\n);
+	printf ("JAM MULAI  ="); scanf("%i",&jam);
+	printf ("MENIT MULAI	=");scanf("%i",&menit);
+	printf ("DETIK MULAI	=");scanf("%i",detik);
+	Total_detik_mulai=(jam_mulai*3600)+(menit_mulai*)
+	print ("\n \n");
+	
+	printf ("SELESAI\n");
+	printf ("JAM SELESAI	="); scanf("%i,&jam");
+	printf ("MENIT SELESAI	="); scanf("%i,&menit");
+	printf ("DETIK SELESAI	="); scanf("%i,&detik");
+	Total_detik_selesai=(jam_selesai*3600)	
+}
