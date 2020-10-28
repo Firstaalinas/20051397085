74bac7e9569711d7b6ff5cc812c3872f44a440f9
Firsta alina s <72842581+Firstaalinas@users.noreply.github.com>
2020-10-28 15:41:09 +0700

    Add files via upload

---
+++ 6.tabel perkalian.cpp
@@ -0,0 +1,34 @@
+//Firsta Alina Saputra
+//NIM : 20051397085
+//MI 2020A
+
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+main()
+{	
+	cout<<"----------------------";
+	cout<<"Tabel Perkalian";
+	cout<<"----------------------";
+	int x[10]={1,2,3,4,5,6,7,8,9,10};
+	int y[10]={1,2,3,4,5,6,7,8,9,10};
+		char ascii;
+endl(cout);
+{
+	cout<<(char)196;
+}
+endl(cout);
+
+for (int a=0; a<10; a++)
+{endl(cout);
+
+for (int j=0; j<10; j++)
+{
+	cout<<setw(5)<<x[a]*y[j];}
+endl(cout);
+}
+endl(cout);
+system("pause");
+}
+
