30272ce6605c9c09b4bc6ff65a498a98da923cdd
Firsta alina s <72842581+Firstaalinas@users.noreply.github.com>
2020-10-28 21:47:43 +0700

    Add files via upload

---
+++ 4. program menghitung jumlah 6 suku pertama.cpp
@@ -0,0 +1,31 @@
+//firsta alina saputra
+//nim : 20051397085
+//MI 2020A
+
+#include<iostream>
+#include<conio.h>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+	float a,b,n;
+	a=1;
+	b=1;
+	n=sqrt(a);
+	
+	int i,kuadrat,barisan,deret;
+
+	cout<<"MENGHITUNG JUMLAH SUKU PERTAMA \n";
+	cout<<"---------------------------------- \n";
+	cout<<"Masukkan Suku Pertama =";
+	cin>>a;kuadrat;
+	cout<<"Masukkan Beda Suku =";
+	cin>>b;kuadrat;
+	cout<<"Masukkan Suku ke-n =";
+	cin>>n;
+	cout<<"Barisan Aritmatika ="
+	<<barisan<<endl;
+	cout<<"deret arimatika ="<<deret;
+}
